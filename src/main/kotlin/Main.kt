9f7eeb7ee4025578f16f82492171345ef9c16f15
unknown <gabriel_hidekii@hotmail.com>
2022-05-17 23:20:09 -0300

    Utilizando estruturas condicionais

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,4 +7,18 @@ fun main(args: Array<String>) {
     println("titular $titular")
     println("numeroDeConta $numeroDeConta")
     println("saldo R$ $saldo")
+
+    if (saldo > 0)
+        println("Conta é positiva")
+    else if (saldo == 0.0)
+        println("Conta é neutra")
+    else
+        println("Conta é negativa")
+
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
+
 }
